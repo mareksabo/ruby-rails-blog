d159aefb4734bffbae68317465db68493e30d59b
Marek Sabo <marek.sabo.gvpt@gmail.com>
2015-12-13 16:50:54 +0100

    Config updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => 'utf-8'
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'serene-wave-1873.herokuapp.com', :protocol => 'http' }
+
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
   # Code is not reloaded between requests.
