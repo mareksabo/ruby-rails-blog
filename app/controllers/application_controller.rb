3f6081c1225b82a5602c1eead202436fa028f0b2
Marek Sabo <marek.sabo.gvpt@gmail.com>
2015-12-13 21:11:05 +0100

    Controller updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,22 +7,13 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
 
-end
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
-
   before_filter do
     resource = controller_name.singularize.to_sym
     method = "#{resource}_params"
     params[resource] &&= send(method) if respond_to?(method, true)
   end
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
 
   protected
 
